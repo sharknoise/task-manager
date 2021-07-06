eea7a5e67d4d1baa54e14a31a0ef39eaf91f2b4b
sharknoise <sharknoise@gmail.com>
2021-07-06 23:42:58 +0300

    add ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (getenv('DEBUG') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.localhost',
+    '127.0.0.1',
+    'sharknoise-task-manager.herokuapp.com',
+]
 
 
 # Application definition
@@ -78,12 +82,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# dj-datbase-url setting
-
-DATABASES = {}
-DATABASE_URL = getenv("DATABASE_URL")
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
