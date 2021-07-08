a5c33db7a52f92b39b8957a748fcc3a7d3ba5ee5
sharknoise <sharknoise@gmail.com>
2021-07-09 00:16:19 +0300

    switch to a separate tasks app

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task_manager import views
+
+from tasks import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.IndexView.as_view()),
+    path('admin/', admin.site.urls),
 ]
--- tasks/apps.py
+++ tasks/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TaskManagerConfig(AppConfig):
+class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task_manager'
+    name = 'tasks'
