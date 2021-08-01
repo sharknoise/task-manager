0c45216628ab9fb5295bab5122efbccc5797266b
sharknoise <sharknoise@gmail.com>
2021-08-01 22:14:44 +0300

    add status creation UI

--- statuses/urls.py
+++ statuses/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from statuses import views
 
 urlpatterns = [
-    path('', views.StatusesListView.as_view(), name='statuses'),
+    path('', views.StatusesListView.as_view(), name='statuses_list'),
+    path('create/', views.StatusCreateView.as_view(), name='status_create'),
 ]
--- statuses/views.py
+++ statuses/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from statuses.models import Status
@@ -6,3 +7,19 @@ from statuses.models import Status
 class StatusesListView(generic.ListView):
     model = Status
     template_name = 'statuses/statuses-list.html'
+
+
+class StatusCreateView(generic.edit.CreateView):
+    model = Status
+    fields = ['name']
+    template_name = 'statuses/status-create.html'
+    success_url = reverse_lazy('statuses_list')
+
+    def form_valid(self, form):
+        """Add the user to the form as the author of the status.
+
+        We are overriding form_valid() because it is the method
+        that eventually returns HttpResponseRedirect().
+        """
+        form.instance.author = self.request.user
+        return super().form_valid(form)
