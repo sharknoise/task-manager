e88525d03e1d606d029de022db74802ecb5878c9
sharknoise <sharknoise@gmail.com>
2021-08-02 00:10:11 +0300

    add check and success message to status creation

--- statuses/views.py
+++ statuses/views.py
@@ -1,7 +1,13 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
 from django.views import generic
 
 from statuses.models import Status
+from task_manager.mixins import (
+    LoginRequiredRedirectMixin,
+    NoPermissionRedirectMixin,
+)
 
 
 class StatusesListView(generic.ListView):
@@ -9,11 +15,17 @@ class StatusesListView(generic.ListView):
     template_name = 'statuses/statuses-list.html'
 
 
-class StatusCreateView(generic.edit.CreateView):
+class StatusCreateView(
+    NoPermissionRedirectMixin,
+    LoginRequiredRedirectMixin,
+    SuccessMessageMixin,
+    generic.edit.CreateView,
+):
     model = Status
     fields = ['name']
     template_name = 'statuses/status-create.html'
     success_url = reverse_lazy('statuses_list')
+    success_message = _('The status has been created.')
 
     def form_valid(self, form):
         """Add the user to the form as the author of the status.
