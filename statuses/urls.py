6bfdefb3172db82818380049fe70d7dfcd30ee18
sharknoise <sharknoise@gmail.com>
2021-08-02 18:15:05 +0300

    add status deletion

--- statuses/urls.py
+++ statuses/urls.py
@@ -5,4 +5,9 @@ from statuses import views
 urlpatterns = [
     path('', views.StatusesListView.as_view(), name='statuses_list'),
     path('create/', views.StatusCreateView.as_view(), name='status_create'),
+    path(
+        '<int:pk>/delete/',
+        views.StatusDeleteView.as_view(),
+        name='status_delete',
+    ),
 ]
--- statuses/views.py
+++ statuses/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
@@ -28,10 +29,32 @@ class StatusCreateView(
     success_message = _('The status has been created.')
 
     def form_valid(self, form):
-        """Add the user to the form as the author of the status.
+        """
+        Add the user to the form as the author of the status.
 
         We are overriding form_valid() because it is the method
         that eventually returns HttpResponseRedirect().
         """
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class StatusDeleteView(
+    NoPermissionRedirectMixin,
+    LoginRequiredRedirectMixin,
+    generic.edit.DeleteView,
+):
+    model = Status
+    template_name = 'statuses/status-delete.html'
+    success_url = reverse_lazy('statuses_list')
+    success_message = _('The status has been deleted.')
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Add success message to messages.
+
+        We need this instead of SuccessMessageMixin which
+        can't be used with DeleteView.
+        """
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
