676882bff35ee8c761db88606cbe0542d526fbbb
sharknoise <sharknoise@gmail.com>
2021-08-03 22:14:36 +0300

    add task creation

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from tasks import views
 
 urlpatterns = [
     path('', views.TasksListView.as_view(), name='tasks_list'),
+    path('create/', views.TaskCreateView.as_view(), name='task_create'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,13 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
 from django.views import View, generic
 
+from task_manager.mixins import (
+    LoginRequiredRedirectMixin,
+    NoPermissionRedirectMixin,
+)
 from tasks.models import Task
 
 
@@ -13,3 +20,20 @@ class IndexView(View):
 class TasksListView(generic.ListView):
     model = Task
     template_name = 'tasks/tasks-list.html'
+
+
+class TaskCreateView(
+    NoPermissionRedirectMixin,
+    LoginRequiredRedirectMixin,
+    SuccessMessageMixin,
+    generic.edit.CreateView,
+):
+    model = Task
+    fields = ['name', 'status', 'description', 'executor']
+    template_name = 'tasks/task-create.html'
+    success_url = reverse_lazy('tasks_list')
+    success_message = _('You have created a new task!')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
