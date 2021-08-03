9e1d7a39b93837d1a6416558456957b6044787a0
sharknoise <sharknoise@gmail.com>
2021-08-03 14:05:27 +0300

    add tests

--- statuses/tests.py
+++ statuses/tests.py
@@ -1,3 +1,46 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from statuses.models import Status
+from users.models import UserModel
+
+
+class StatusesTest(TestCase):
+
+    def setUp(self):
+        self.tester = UserModel.objects.create_user(
+            username='tester',
+            password='Abc123+-=',
+        )
+        self.test_status = Status.objects.create(
+            name='test_status',
+            author=self.tester,
+        )
+        self.client.login(username='tester', password='Abc123+-=')
+
+    def test_create(self):
+        response = self.client.post(
+            '/statuses/create/',
+            {'name': 'tested'},
+            follow=True,
+        )
+        self.assertRedirects(response, reverse('statuses_list'))
+        self.assertEqual(
+            'tested',
+            Status.objects.get(name='tested').name,
+        )
+
+    def test_update(self):
+        self.client.post(
+            reverse('status_update', args=str(self.test_status.id)),
+            {'name': 'renamed'},
+        )
+        self.test_status.refresh_from_db()
+        self.assertEqual(self.test_status.name, 'renamed')
+
+    def test_delete(self):
+        test_status_id = self.test_status.id
+        self.client.post(reverse('status_delete', args=str(test_status_id)))
+        with self.assertRaises(ObjectDoesNotExist):
+            Status.objects.get(id=test_status_id)
--- users/tests.py
+++ users/tests.py
@@ -49,3 +49,59 @@ class UsersTest(TestCase):
         )
         self.assertTrue(response.context['request'].user.is_authenticated)
         self.assertRedirects(response, '/')
+
+    def test_update(self):
+        self.client.login(username='tester', password='Abc123+-=')
+        self.client.post(
+            reverse('update', args=str(self.tester.id)), {
+                'username': 'Тестировщик',
+                'first_name': 'Иван',
+                'last_name': 'Иванов',
+                'email': 'ivan@mail.ru',
+                'password1': 'Abc123+-=',
+                'password2': 'Abc123+-=',
+            },
+        )
+        self.tester.refresh_from_db()
+        self.assertEqual(self.tester.username, 'Тестировщик')
+        self.assertEqual(self.tester.first_name, 'Иван')
+        self.assertEqual(self.tester.last_name, 'Иванов')
+        self.assertEqual(self.tester.email, 'ivan@mail.ru')
+
+    def test_guest_permissions_redirect(self):
+        response = self.client.post(
+            '/users/1/delete/',
+            follow=True,
+        )
+        self.assertRedirects(
+            response,
+            reverse('login') + '?next=/users/1/delete/',
+        )
+
+    def test_user_permissions(self):
+        self.test_user = UserModel.objects.create_user(
+            username='test_user',
+            password='testuserpassword',
+        )
+        self.client.login(username='tester', password='Abc123+-=')
+        response = self.client.post(
+            reverse('update', args=str(self.test_user.id)), {
+                'username': 'renamed_test_user',
+                'first_name': 'xyz',
+                'last_name': 'xyz',
+                'email': 'xyz@xyz.com',
+                'password1': 'xyz12345',
+                'password2': 'xyz12345',
+            },
+            follow=True,
+        )
+        self.assertRedirects(response, '/users/')
+        self.assertEqual(self.test_user.username, 'test_user')
+        response = self.client.post(
+            reverse('delete', args=str(self.test_user.id)),
+            follow=True,
+        )
+        self.assertRedirects(response, '/users/')
+        self.assertTrue(
+            UserModel.objects.filter(id=self.test_user.id).exists(),
+        )
